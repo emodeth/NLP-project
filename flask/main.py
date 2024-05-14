c4806566c432774b7d5c8b9b11217a67732fc449
Cem Canbolat <cemcanbolat159@gmail.com>
2024-05-14 15:55:30 +0300

    Restful added

--- flask/main.py
+++ flask/main.py
@@ -3,19 +3,17 @@ from fileinput import filename
 from flask import *  
 from pypdf import PdfReader 
 from flask_cors import CORS
+from flask_restful import Resource, Api
 
-app = Flask(__name__)  
+
+app = Flask(__name__)
+api = Api(app)
 CORS(app, origins='http://localhost:3000')
   
-@app.route('/')   
-def main():   
-    return Response("{'Status':'OK'}",status=200)
-  
-@app.route('/upload', methods = ['POST'])   
-def upload_and_process():
-    """Handles file upload and text extraction from the first page of a PDF."""
 
-    if request.method == 'POST':
+class Upload(Resource):
+
+    def post(self):
         f = request.files.get('file')
 
         if f is None or f.filename == '':
@@ -24,16 +22,22 @@ def upload_and_process():
             reader = PdfReader(f)
             page = reader.pages[0]
             text = page.extract_text()
+
+            data = {
+                'Status': 202,
+                'Text': text
+            }
         
-            return Response(f"{{'Status': 202, 'Text': '{text}'}}", status=202)
+            return jsonify(data)
 
 
         except Exception as e:
             print(f"Error processing file: {e}")
             return Response("{'Status': 'Error', 'Message': 'Internal server error'}", status=500)
 
-    else:
-        return Response("{'Status': 'Error', 'Message': 'Method not allowed'}", status=405)
+  
+api.add_resource(Upload, '/upload')
+
   
 if __name__ == '__main__':   
     app.run(debug=True)
